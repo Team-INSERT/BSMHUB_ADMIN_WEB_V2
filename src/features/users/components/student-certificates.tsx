62069df8a8a82cbff615669afd875da3e12ad2ed
gamzamandu <galbimandu1126@gmail.com>
2026-03-17 12:02:52 +0900

    chore : 자격증 전공 여부 상관없이 통합

--- src/features/users/components/student-certificates.tsx
+++ src/features/users/components/student-certificates.tsx
@@ -1,8 +1,4 @@
 import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { useEditUser } from '../context/edit-context'
 import { UserDetailType } from '../data/schema'
 
 export const StudentCertificates = ({
@@ -10,110 +6,15 @@ export const StudentCertificates = ({
 }: {
   datas: UserDetailType['student_certificates']
 }) => {
-  const { editingSection } = useEditUser()
-
-  const certificatesGroup = [
-    {
-      title: '전공',
-      items: datas.filter((data) => data.certificates.is_software),
-      is_software: true,
-    },
-    {
-      title: '전공 외',
-      items: datas.filter((data) => !data.certificates.is_software),
-      is_software: false,
-    },
-  ]
-
   return (
-    <div>
-      {editingSection === 'certificates' ? (
-        <div className='space-y-4'>
-          <div className='space-y-2'>
-            <Label>전공 자격증</Label>
-            <div className='flex items-center gap-2'>
-              <Input
-                placeholder='전공 자격증 추가'
-                // value={editData.newMajorCert || ""}
-                onChange={() => {}}
-              />
-              <Button size='sm' onClick={() => {}}>
-                추가
-              </Button>
-            </div>
-            <div className='mt-2 flex flex-wrap gap-2'>
-              {/* {editData.certifications?.major?.map((cert, idx) => (
-                  <Badge key={idx} className="flex items-center gap-1">
-                    {cert}
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="h-4 w-4 rounded-full"
-                      onClick={() => {}}
-                    >
-                      <CircleX size={12} />
-                    </Button>
-                  </Badge>
-                ))} */}
-            </div>
-          </div>
-
-          <div className='space-y-2'>
-            <Label>전공 외 자격증</Label>
-            <div className='flex items-center gap-2'>
-              <Input
-                placeholder='전공 외 자격증 추가'
-                // value={}
-                onChange={() => {}}
-              />
-              <Button size='sm' onClick={() => {}}>
-                추가
-              </Button>
-            </div>
-            <div className='mt-2 flex flex-wrap gap-2'>
-              {/* {editData.certifications?.nonMajor?.map((cert, idx) => (
-                  <Badge
-                    key={idx}
-                    variant="outline"
-                    className="flex items-center gap-1"
-                  >
-                    {cert}
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="h-4 w-4 rounded-full"
-                      onClick={() =>
-                        removeArrayItem("certifications.nonMajor", idx)
-                      }
-                    >
-                      <CircleX size={12} />
-                    </Button>
-                  </Badge>
-                ))} */}
-            </div>
-          </div>
-        </div>
-      ) : (
-        <dl className='space-y-2'>
-          {certificatesGroup.map(({ title, items, is_software }) => (
-            <div className='flex gap-2'>
-              <dt className='w-24 flex-shrink-0 font-medium'>{title}:</dt>
-              <dd className='flex flex-wrap gap-1'>
-                {items.length > 0
-                  ? items.map((item) => (
-                      <Badge
-                        key={item.certificates.certificate_id}
-                        variant={is_software ? 'default' : 'outline'}
-                      >
-                        {item.certificates.certificate_name ?? '-'}
-                      </Badge>
-                    ))
-                  : '-'}
-              </dd>
-            </div>
-          ))}
-        </dl>
-      )}
+    <div className='flex flex-wrap gap-1'>
+      {datas.length > 0
+        ? datas.map((item) => (
+            <Badge key={item.certificates.certificate_id}>
+              {item.certificates.certificate_name ?? '-'}
+            </Badge>
+          ))
+        : '-'}
     </div>
   )
 }
